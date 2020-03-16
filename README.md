753c9bd678a2b495c524db9094c9a3b11c71efd2
Brayan <brayansamsungfame@gmail.com>
2020-03-16 12:32:10 -0400

    Primer Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Aplicacion de comandos: Tareas po Hacer
+
+Descripcion: tiene la misma funcionalidad de un bloc de notas
+se puede crear, actualizar, borrar y cambiar el estado de las tareas q creas
+
+
+## Para instalar:
+npm install
---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    demmand: true,
+    alias: 'd',
+    desc: 'Descripcion de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tearea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crea un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borra una actividad en base a la descripcion', {
+        descripcion,
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
